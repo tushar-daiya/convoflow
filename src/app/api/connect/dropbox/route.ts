9b2391b1ec302aa2c987e2de70656045a438d9b1
Tushar Daiya <tushardaiya@outlook.com>
2025-03-21 21:17:10 +0530

    fixes

--- src/app/api/connect/dropbox/route.ts
+++ src/app/api/connect/dropbox/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const redirect_uri = "http://localhost:3000/api/auth/callback/dropbox";
+  const redirect_uri = `${process.env.FRONTEND_URL}/api/auth/callback/dropbox`;
   const scope =
     "account_info.read files.content.read files.metadata.read file_requests.read";
   const url = `https://www.dropbox.com/oauth2/authorize?client_id=${process.env.AUTH_DROPBOX_ID}&response_type=code&redirect_uri=${redirect_uri}&scope=${scope}&token_access_type=offline`;
